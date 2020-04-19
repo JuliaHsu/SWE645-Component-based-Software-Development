f4db7d7ea172fad63c353add8032816620ecb8e3
POkilo <poyen.nctu@gmail.com>
2020-04-19 00:37:28 -0400

    add two folder

---
+++ hw3/swe645-app/src/app/show-survey/show-survey.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { Survey } from '../survey-interface.service';
+import { Observable } from 'rxjs';
+import { catchError, map, tap, finalize, retry } from 'rxjs/operators';
+@Component({
+  selector: 'app-show-survey',
+  templateUrl: './show-survey.component.html',
+  styleUrls: ['./show-survey.component.css']
+})
+export class ShowSurveyComponent implements OnInit {
+
+  url: string = 'http://localhost:8080/swe645-restful-api/restful/survey'
+  surveys: Survey[]
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+    this.getSurvey()
+    .subscribe(
+      res=>{
+        console.log(res);
+        this.surveys = res['body'];
+      },
+      error=>alert('not able to request the content')
+      );
+  }
+
+  getSurvey():Observable<any>{
+    return this.http.get(this.url, {observe:'response'});
+  }
+
+}

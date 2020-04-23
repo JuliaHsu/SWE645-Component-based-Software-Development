064502e7ff0cbef80d79fbf2147894a82ead0a30
POkilo <poyen.nctu@gmail.com>
2020-04-23 15:48:29 -0400

    test

--- hw3/swe645-app/src/app/show-survey/show-survey.component.ts
+++ hw3/swe645-app/src/app/show-survey/show-survey.component.ts
@@ -11,7 +11,7 @@ import { catchError, map, tap, finalize, retry } from 'rxjs/operators';
 })
 export class ShowSurveyComponent implements OnInit {
 
-  url: string = 'http://ec2-52-23-240-116.compute-1.amazonaws.com:30220/swe645-restful-api/restful/survey'
+  url: string = 'http://ec2-34-227-13-21.compute-1.amazonaws.com:32593/swe645-restful-api/restful/survey'
   surveys: Survey[]
   constructor(private http:HttpClient) { }
 
--- hw3/swe645-app/src/app/survey-form/survey-form.component.ts
+++ hw3/swe645-app/src/app/survey-form/survey-form.component.ts
@@ -15,7 +15,7 @@ import { CheckboxRequiredValidator } from '@angular/forms';
 })
 
 export class SurveyFormComponent implements OnInit {
-  url: string = 'http://ec2-52-23-240-116.compute-1.amazonaws.com:30220/swe645-restful-api/restful/survey/create'
+  url: string = 'http://ec2-34-227-13-21.compute-1.amazonaws.com:32593/swe645-restful-api/restful/survey/create'
   message:Survey = new Survey()
   campus_value:string[] = ['students','location','campus','dorm rooms','atmosphere','sports'] 
   today: string = new Date().toDateString();
